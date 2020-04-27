50d1c724859cdad0752e3c991f941237baefa1a1
David <dgkilolo23@gmail.com>
2020-04-27 09:35:27 +0300

    adds the route that will display the artciles from a particular source.

--- app/views.py
+++ app/views.py
@@ -17,13 +17,13 @@ def index():
     title = 'News Updates'
     return render_template('index.html', title = title, sources = sources )
 
-# @app.route('/search/<source_id>')
-# def sourceArticles(source_id):
-#     '''
-#     Function that displays the articles from a particluar source.
-#     '''
-#     article_results = get_articles(source_id)
+@app.route('/search/<source_id>')
+def sourceArticles(source_id):
+    '''
+    Function that displays the articles from a particluar source.
+    '''
+    article_results = get_articles(source_id)
 
-#     title =  source_id
+    title =  source_id
 
-#     return render_template("article.html",title = title, articles = article_results)
+    return render_template("article.html",title = title, articles = article_results)
