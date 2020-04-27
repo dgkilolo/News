b34e06137b9de62a54085adb353198904a58034f
David <dgkilolo23@gmail.com>
2020-04-27 09:28:18 +0300

    adds a variable sourcese in the views file that will allow access to the get_news output in the index.html file.

--- app/views.py
+++ app/views.py
@@ -3,19 +3,19 @@ from app import app
 from .requests import get_news,get_articles
 
 # Views
-# @app.route('/')
-# def index():
+@app.route('/')
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
+    '''
+    View root page function that returns the index page and its data
+    '''
 
-#     # Getting the sources
-#     sources = get_news()
-#     print(sources)
+    # Getting the sources
+    sources = get_news()
+    print(sources)
 
-#     title = 'News Updates'
-#     return render_template('index.html', title = title, sources = sources )
+    title = 'News Updates'
+    return render_template('index.html', title = title, sources = sources )
 
 # @app.route('/search/<source_id>')
 # def sourceArticles(source_id):
