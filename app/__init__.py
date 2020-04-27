3413020f1899b20949875f06d91253c0eeb8349c
David <dgkilolo23@gmail.com>
2020-04-27 15:06:35 +0300

    changes the applications file structure.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from .config import config_options
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -11,6 +11,24 @@ app.config.from_pyfile('config.py')
 
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Will add the views and forms
+
+    return app
 
 from app import views
 from app import error
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
     '''
     General configuration parent class
@@ -26,3 +28,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
