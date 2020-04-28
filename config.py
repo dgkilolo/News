98c57d984920678dedaa041d18d887d851d02497
David <dgkilolo23@gmail.com>
2020-04-28 08:40:24 +0300

    changes api key location.

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_KEY = 'cd613b4357b64293bb888c20c9aa8cea'
     NEWS_API_BASE_URL = 'http://newsapi.org/v2/sources?&apiKey={}'
     NEWS_API_ARTICLES_URL = 'http://newsapi.org/v2/{}?sources={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 
